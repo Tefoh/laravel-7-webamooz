a526e5ad82c9089387889d5371ed66f2dfa26fa5
tefo <tefo.ha27@gmail.com>
2020-03-12 10:50:06 +0330

    components

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <x-login-form role="author" class="bg-danger" />
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <x-auth.register-form  title="webamooz"  data="asdasdasd"></x-auth.register-form>
+        </div>
+    </div>
+</div>
+@endsection
