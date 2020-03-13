41ccb08141c8bf5c9b6b343f5901ad58617e35f0
tefo <tefo.ha27@gmail.com>
2020-03-13 23:05:39 +0330

    mail

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return new \App\Mail\UserVerified();
 });
 
 Route::get('/categories/{category}/posts/{post:id}', function (Category $category, Post $post) {
